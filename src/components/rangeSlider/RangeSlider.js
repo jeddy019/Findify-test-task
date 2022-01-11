03429a1ebed87ea4eed1be50cebc7c41e7af8b10
Your Name <you@example.com>
2022-01-10 16:45:08 -0800

    updated

--- src/components/rangeSlider/RangeSlider.js
+++ src/components/rangeSlider/RangeSlider.js
@@ -1,34 +1,23 @@
 import React, { Component } from 'react';
-import './RangeSlider.css';
 
 class RangeSlider extends Component {
-    state = { value: 3 }
+    state = { value: 250 }
   
   handleChange = (event) => this.setState({value: event.target.value});
 
   render() {
     return (
       <div>
-         <input
-        type="range"
-        min="0"
-        max="1000"
-        onChange={this.handleChange}
-        className="thumb thumb--zindex-3"
-      />
-          {this.state.value}
-         <input
-        type="range"
-        min="0"
-        max="1000"
-        onChange={this.handleChange}
-        className="thumb thumb--zindex-4"
-      />
-      {this.state.value}
-        <div className="slider">
-       <div className="slider__track" />
-       <div className="slider__range" />
-        </div>
+        <label>
+          <input 
+            id="typeinp" 
+            type="range" 
+            min="1.0" max="500.0" 
+            value={this.state.value} 
+            onChange={this.handleChange}
+            step="1"/>
+          ${this.state.value}
+        </label>
       </div>
     );
   }
